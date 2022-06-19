b900a34bb50ac91d1e0c74ed5a9883a6a12231e8
Jens Deubach <jens-d@live.de>
2022-06-19 23:24:48 +0200

    switch IV challenge rdy

--- !Tutorials/Kontrollstrukturen/switch-anweisung IV Challenge/main.js
+++ !Tutorials/Kontrollstrukturen/switch-anweisung IV Challenge/main.js
@@ -3,16 +3,18 @@
 let produkt = "Schokol2ade";
 
 switch (produkt) {
-    case "Milch":    
+    case "Brot":
+        console.log(`${produkt} kostet 1,89€`);
+        break;
+
+    case "Milch":
     case "Apfel":
         console.log(`${produkt} kostet 0,79€`);
         break;
+        
     case "Schokolade":
     case "Chips":
         console.log(`${produkt} kostet 0,99€`);
-        break;
-    case "Brot":
-        console.log(`${produkt} kostet 1,89€`);
         break;
 
     default:
