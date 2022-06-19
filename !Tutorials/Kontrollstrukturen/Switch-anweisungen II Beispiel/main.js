4251372379d17558e33434d8a19f391ab5f346f7
Jens Deubach <jens-d@live.de>
2022-06-19 23:12:18 +0200

    Switch III rdy

--- !Tutorials/Kontrollstrukturen/Switch-anweisungen II Beispiel/main.js
+++ !Tutorials/Kontrollstrukturen/Switch-anweisungen II Beispiel/main.js
@@ -1,10 +1,18 @@
 "use strict";
 
-switch (key) {
-    case value:
-        
-        break;
+let ein_name = "David";
 
+switch (ein_name) {
+    case "Jens":
+        console.log("Hallo Jens!");
+        
+    case "Martin":
+        console.log("Hallo Martin!");
+        break;
+    case "David":
+        console.log("Hallo David!");
+        break;
     default:
+        console.log("Du bist unbekannt!");
         break;
 }
