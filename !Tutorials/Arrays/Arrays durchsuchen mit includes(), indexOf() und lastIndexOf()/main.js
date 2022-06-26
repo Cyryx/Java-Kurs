e444f5461a20f776755e1bd5ab19f408a6ca4bae
Jens Deubach <jens-d@live.de>
2022-06-26 21:58:58 +0200

    Arrays

---
+++ !Tutorials/Arrays/Arrays durchsuchen mit includes(), indexOf() und lastIndexOf()/main.js
@@ -0,0 +1,47 @@
+"use strict";
+
+let personen_array = [
+    "Kathrin",
+    "Jens",    
+    "Sonja",
+    "Elina",
+    "Maik",
+    "Bente",
+    "Hartmut",
+    "Leon",
+    "Elina",
+    "Laura"
+]
+
+console.log(personen_array);
+
+//array.includes(): Duchsucht das Array ab einem Startindex nach einen Wert und gibt True zurück, 
+//wenn das Element gefunden wurde, bzw. false, wenn das Element nicht gefunden wurde
+//Syntax: array.includes(Suchwert[, Startindex])
+//nicht destuktiv
+console.log('***********************************************************************');
+let name_var = "Elina";
+switch (personen_array.includes(name_var, 0)) {
+    case true:
+        console.log(`${personen_array[personen_array.indexOf(name_var,0)]} wurde an stelle personen_array[${personen_array.indexOf(name_var,0)}] des Arrays gefunden.`)
+        break;
+    case false:
+        console.log(`Person wurde nicht gefunden.`)
+        break;
+}
+console.log('***********************************************************************');
+
+//array.indexOf(): Durchsucht das Array ab einem Startindex nach einem Wert und gibt den Index der ersten Fundstelle zurück,
+// wenn der Wert gefunden wurde oder -1 wenn der Wert im Array nicht gefunden wurde
+//Syntax: array.indexOf(Suchwert[,Startindex])
+//nicht destuktiv
+
+console.log(`${personen_array[personen_array.indexOf(name_var,0)]} befindet sich an Stelle personen_array[${personen_array.indexOf(name_var,0)}] von indexOf.`);
+console.log('***********************************************************************');
+
+//array.lastIndexOf(): Durchsucht das Array ab einem Startindex Rückwärts nach einem Wert und gibt den Index der ersten Fundstelle zurück,
+//wenn der Wert gefunden wurde oder -1 wenn der Wert im Array nicht gefunden wurde
+//Syntax: array.lastIndexOf(Suchwert[,Startindex])
+//nicht destuktiv
+console.log(`${personen_array[personen_array.lastIndexOf(name_var,10)]} befindet sich an Stelle personen_array[${personen_array.lastIndexOf(name_var,10)}] von lastIndexOf.`);
+console.log('***********************************************************************');
--- !Tutorials/Arrays/Elemente hinzufügen und entfernen/main.js
+++ !Tutorials/Arrays/Elemente hinzufügen und entfernen/main.js
@@ -1,2 +1,70 @@
 "use strict";
 
+let personen_array = [
+    "Maik",
+    "Jens",
+    "Kathrin",
+    "Bente",
+    "Hartmut",
+    "Sonja",
+    "Elina",
+    "Leon",
+    "Laura"
+]
+//
+console.log('***********************************************************************');
+
+console.log(personen_array);
+
+console.log('***********************************************************************');
+
+console.log(`Person geändert von ${personen_array[0]} zu ${personen_array[0] = "Benny"}`);
+
+console.log('***********************************************************************');
+
+console.log(personen_array);
+
+console.log('***********************************************************************');
+
+
+//push() hinzufügen eines Elements am ende des Arrays
+personen_array.push("Maik");
+
+console.log(`Person wurde am ende des Arrays hinzugefügt: ${personen_array[personen_array.length-1]}`);
+
+console.log('***********************************************************************');
+
+console.log(personen_array);
+
+console.log('***********************************************************************');
+//
+
+//pop() Methode letzten eintrag entfernen 
+console.log(`
+Letzte Person im Array gelöscht = personen_array.pop();
+${personen_array}  
+${personen_array.pop()} gelöscht
+${personen_array} 
+`);
+
+console.log('***********************************************************************');
+//unshift() fügt am Anfang des Arrays einen Wert zu 
+console.log(`
+Person am anfang des Array hinzugefügt = personen_array.unshift();
+${personen_array}  
+${personen_array.unshift("Maik")} hinzugefügt
+${personen_array} 
+`);
+
+console.log('***********************************************************************');
+
+//shift() methode entfernt das erste Element des Arrays
+console.log(`
+Person am anfang des Array gelöscht = personen_array.shift();
+${personen_array}  
+${personen_array.shift()} gelöscht
+${personen_array} 
+`);
+
+console.log('***********************************************************************');
+
