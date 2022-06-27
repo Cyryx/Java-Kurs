89478f4144392bf1ad481885e8d9ccfb2daea77c
Jens Deubach <jens-d@live.de>
2022-06-27 23:27:42 +0200

    schleifen

--- !Tutorials/Schleifen/Arrays durchlaufen mit der forEach-Methode/main.js
+++ !Tutorials/Schleifen/Arrays durchlaufen mit der forEach-Methode/main.js
@@ -1,14 +1,35 @@
 "use strict";
 
 let personen_array = [
-`Kathrin`,
-`Jens`,
-`Sonja`,
-`Elina`,
-`Maik`,
-`Bente`,
-`Hartmut`,
-`Leon`,
-`Elina`,
-`Laura`
+    `Kathrin`,
+    `Jens`,
+    `Sonja`,
+    `Elina`,
+    `Maik`,
+    `Bente`,
+    `Hartmut`,
+    `Leon`,
+    `Elina`,
+    `Laura`,
+    `Mia`
 ];
+//console.log(personen_array);
+
+/* 
+for (let i = 0; i < personen_array.length; i++) {
+    const element = personen_array[i];
+    console.log(element);
+} 
+*/
+/* 
+let meine_funktion = function (e) {
+    console.log(e);
+}
+
+personen_array.forEach(meine_funktion) 
+*/
+
+
+personen_array.forEach(function (e) {
+    console.log(e);
+})
