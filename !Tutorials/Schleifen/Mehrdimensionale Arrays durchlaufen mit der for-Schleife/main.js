9fba32dbf8d1e4648e7e860689d39ca898f664a2
Jens Deubach <jens-d@live.de>
2022-06-27 22:58:29 +0200

    schleifen

--- !Tutorials/Schleifen/Mehrdimensionale Arrays durchlaufen mit der for-Schleife/main.js
+++ !Tutorials/Schleifen/Mehrdimensionale Arrays durchlaufen mit der for-Schleife/main.js
@@ -1,2 +1,25 @@
 "use strict";
 
+let waren = [
+    [
+        "Äpfel",
+        "Birnen",
+        "Bananen"
+    ],
+    [
+        "Möhren",
+        "Sellerie",
+        "Kohl"
+    ],
+    [
+        "Graubrot",
+        "Schwarzbrot",
+        "Vollkornbrot"
+    ]
+];
+
+for (let i = 0; i < waren.length; i++) {
+    for (let x = 0; x < waren[i].length; x++) {
+        console.log(`Ich mag ${waren[i][x]}.`);
+    }
+}
