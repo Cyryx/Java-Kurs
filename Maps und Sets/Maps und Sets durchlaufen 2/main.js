4c7e215f661e3b8cbabc01c6c4434f72373099db
Jens Deubach <jens-d@live.de>
2022-07-05 23:55:19 +0200

    Maps und Sets durchlaufen 2

--- Maps und Sets/Maps und Sets durchlaufen 2/main.js
+++ Maps und Sets/Maps und Sets durchlaufen 2/main.js
@@ -10,10 +10,14 @@ mein_set.add(2);
 mein_set.add(1);
 mein_set.add(0);
 
+for (let wert of mein_set.values()) {
+    console.log(wert);
+};
 
 // console.log(mein_set.entries());
 // console.log(mein_set.keys());
 // console.log(mein_set.values());
+
 //Map
 let meine_map = new Map();
 meine_map.set(0, "Wert der Eigenschaft 0");
@@ -22,9 +26,11 @@ meine_map.set(2, "Wert der Eigenschaft 2");
 meine_map.set(3, "Wert der Eigenschaft 3");
 meine_map.set(4, "Wert der Eigenschaft 4");
 meine_map.set(5, "Wert der Eigenschaft 5");
+
 /* for (let paar of meine_map.entries()) {
     console.log(paar);
 } */
+console.log('********************');
 
 for (let [eigenschaft,wert] of meine_map.entries()) {
     console.log(eigenschaft);
