d7fdd365c155072a7f987f95e933dc39f83c0c4a
Jens Deubach <jens-d@live.de>
2022-07-06 11:42:22 +0200

    Objekt => Map

--- !Praxis/Einnahmen+und+Ausgaben+unterscheiden/src/js/main.js
+++ !Praxis/Einnahmen+und+Ausgaben+unterscheiden/src/js/main.js
@@ -1,31 +1,36 @@
 "use strict";
-let min = 1,
+let min = 0,
     datum_tag_min = 10,
-    datum_monat_min = 10;
+    datum_monat_min = 10,
+    datum_jahr_min = 10;
+
 let max = 10000,
     datum_tag_max = 30,
-    datum_monat_max = 12;
+    datum_monat_max = 12,
+    datum_jahr_max = 99;
+
 let typ = ["Ausgabe", "Einnahme"]
 
 const haushaltsbuch = {
 
-    gesamtbilanz: {
-        einnahmen: 0,
-        ausgaben: 0,
-        bilanz: 0
-    },
+    gesamtbilanz: new Map(),
 
-    neuer_eintrag: {
-        titel: null,
-        typ: null,
-        betrag: null,
-        datum: null
-    },
     eintraege: [],
 
+    eintrag_erfassen() { //DEMO
+        //Daten erfassen
+        let neuer_eintrag = new Map();
+        let zufallswert = Math.floor(Math.random() * (1 - 0 + 1) + 0);
+        console.log(zufallswert)
 
+        neuer_eintrag.set("titel", "Test");
+        neuer_eintrag.set(`typ`, typ[zufallswert]);
+        neuer_eintrag.set(`betrag`, parseInt(Math.floor(Math.random() * (max - min + 1)) + min));
+        neuer_eintrag.set(`datum`, `20${Math.floor(Math.random() * (datum_jahr_max - datum_jahr_min + 1)) + datum_jahr_min}-${Math.floor(Math.random() * (datum_monat_max - datum_monat_min + 1)) + datum_monat_min}-${Math.floor(Math.random() * (datum_tag_max - datum_tag_min + 1)) + datum_tag_min}`);
 
-    eintrag_erfassen() {
+        this.eintraege.push(neuer_eintrag);
+    },
+    /* eintrag_erfassen() { //Original
         //Daten erfassen
         this.eintraege.push(
             {
@@ -38,9 +43,20 @@ const haushaltsbuch = {
             }
         )
 
-    },
+    }, */
 
-    eintraege_sortieren() {
+    eintraege_sortieren() {//Demo
+        this.eintraege.sort(function (a, b) {
+            if (a.get("datum") > b.get("datum")) {
+                return -1;
+            } else if (a.get("datum") < b.get("datum")) {
+                return 1;
+            } else {
+                return 0;
+            } 
+        })
+    },
+    /*eintraege_sortieren() {//Original
         this.eintraege.sort(function (a, b) {
             if (a.datum > b.datum) {
                 return -1;
@@ -50,21 +66,56 @@ const haushaltsbuch = {
                 return 0;
             }
         })
-    },
+    }, */
 
-    eintraege_ausgeben() {
+    eintraege_ausgeben() {//Demo
         console.clear();
         this.eintraege.forEach(function (eintrag) {
             console.log(
-                `Titel: ${eintrag.titel}\n`
-                + `Typ: ${eintrag.typ}\n`
-                + `Betrag: ${eintrag.betrag} ct\n`
-                + `Datum: ${eintrag.datum}\n`
+                `Titel: ${eintrag.get("titel")}\n`
+                + `Typ: ${eintrag.get("typ")}\n`
+                + `Betrag: ${eintrag.get("betrag")} ct)\n`
+                + `Datum: ${eintrag.get("datum")}\n`
             );
         });
     },
+    /*     eintraege_ausgeben() {//Original
+            console.clear();
+            this.eintraege.forEach(function (eintrag) {
+                console.log(
+                    `Titel: ${eintrag.titel}\n`
+                    + `Typ: ${eintrag.typ}\n`
+                    + `Betrag: ${eintrag.betrag} ct\n`
+                    + `Datum: ${eintrag.datum}\n`
+                );
+            });
+        }, */
 
-    gesamtbilanz_erstellen() {
+    gesamtbilanz_erstellen() {//Demo
+        //Einnahme/Ausgabe berechnen
+        let neue_gesamtbilanz = new Map();
+        neue_gesamtbilanz.set("einnahmen", 0);
+        neue_gesamtbilanz.set("ausgaben", 0);
+        neue_gesamtbilanz.set("bilanz", 0);
+
+        this.eintraege.forEach(function (eintrag) {
+            switch (eintrag.get("typ")) {
+                case "Einnahme":
+                    neue_gesamtbilanz.set("einnahmen", neue_gesamtbilanz.get("einnahmen") + eintrag.get("betrag"));
+                    neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") + eintrag.get("betrag"));
+                    break;
+                case "Ausgabe":
+                    neue_gesamtbilanz.set("ausgaben", neue_gesamtbilanz.get("ausgaben") + eintrag.get("betrag"));
+                    neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") - eintrag.get("betrag"));
+                    break;
+                default:
+                    console.log(`Der Typ "${eintrag.get("typ")}" ist nicht bekannt.`);
+                    break;
+            }
+        });
+        this.gesamtbilanz = neue_gesamtbilanz;
+    },
+    /* gesamtbilanz_erstellen() {//Original
         //Einnahme/Ausgabe berechnen
         let neue_gesamtbilanz = {
             einnahmen: 0,
@@ -87,9 +138,18 @@ const haushaltsbuch = {
             }
         });
         this.gesamtbilanz = neue_gesamtbilanz;
-    },
+    }, */
 
-    gesamtbilanz_ausgeben() {
+    gesamtbilanz_ausgeben() {//Demo
+        // Gesamtbilanz ausgaben
+        console.log(
+            `Einnahmen: ${this.gesamtbilanz.get("einnahmen")} ct\n`
+            + `Ausgaben: ${this.gesamtbilanz.get("ausgaben")} ct\n`
+            + `Bilanz: ${this.gesamtbilanz.get("bilanz")} ct\n`
+            + `Bilanz ist positiv: ${this.gesamtbilanz.get("bilanz") >= 0}\n`
+        )
+    },
+    /* gesamtbilanz_ausgeben() {//Original
         // Gesamtbilanz ausgaben
         console.log(
             `Einnahmen: ${this.gesamtbilanz.einnahmen} ct\n`
@@ -97,26 +157,27 @@ const haushaltsbuch = {
             + `Bilanz: ${this.gesamtbilanz.bilanz} ct\n`
             + `Bilanz ist positiv: ${this.gesamtbilanz.bilanz >= 0}\n`
         )
-    },
+    }, */
 
     eintrag_hinzufuegen() {
         let weiterer_eintrag = true;
 
         while (weiterer_eintrag) {
-            this.eintrag_erfassen();
-            this.eintraege_sortieren();
-            this.eintraege_ausgeben();
-            this.gesamtbilanz_erstellen();
-            this.gesamtbilanz_ausgeben();
-            weiterer_eintrag = confirm("Eintrag hinzufügen?");
+            weiterer_eintrag = prompt("Wieviele Einträge hinzufügen?");//confirm("Eintrag hinzufügen?");
+            for (let i = 0; i < weiterer_eintrag; i++) {
+                if (i < weiterer_eintrag) {
+                    this.eintrag_erfassen();
+                    this.eintraege_sortieren();
+                    this.eintraege_ausgeben();
+                    this.gesamtbilanz_erstellen();
+                    this.gesamtbilanz_ausgeben();
+                } else {
+                    return false;
+                }
+            }
         }
-
     }
-
 };
 
-
-
 haushaltsbuch.eintrag_hinzufuegen();
-
 console.log(haushaltsbuch);
