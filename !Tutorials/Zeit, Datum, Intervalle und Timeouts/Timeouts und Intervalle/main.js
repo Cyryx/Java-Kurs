e4cbfd1b0d0894afba458ef530fa1b38218bf867
Jens Deubach <jens-d@live.de>
2022-07-07 22:54:12 +0200

    Timeouts und Intervalle

--- !Tutorials/Zeit, Datum, Intervalle und Timeouts/Timeouts und Intervalle/main.js
+++ !Tutorials/Zeit, Datum, Intervalle und Timeouts/Timeouts und Intervalle/main.js
@@ -1,2 +1,36 @@
 "use strict";
 
+//setTimeout(callback function, zeitspanne[,parameter1,parameter2,parameter3,..., parameterN]);
+console.log('**********Timeout**********');
+
+/* 
+setTimeout(function () {
+    console.log("3!");
+},1000)
+setTimeout(function () {
+    console.log("2!");
+},2000)
+setTimeout(function () {
+    console.log("1!");
+},3000)
+setTimeout(function () {
+    console.log("LOS!");
+},3500)
+ */
+//setTimeout(code-string)
+
+setTimeout("console.log(\"Fertig\");",1000);
+//setTimeout("console.log('Fertig');", 4000);
+
+//clearTimeout(id);
+console.log('**********Intervall**********');
+
+//setIntervall(callback function, zeitspanne[,parameter1,parameter2,parameter3,..., parameterN]);
+/* 
+setInterval(function () {
+    console.log("3!");
+},1000)
+ */
+setInterval("console.log(\"Fertig\");",1000);
+
+setTimeout("clearInterval(4);",4000);
