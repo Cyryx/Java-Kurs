27a51061495ff6390961185356dba15dfee77160
Jens Deubach <jens-d@live.de>
2022-07-08 23:01:11 +0200

    97 Zufallszahlen

--- !Tutorials/Arbeiten mit Strings und Numbers/97 Zufallszahlen/main.js
+++ !Tutorials/Arbeiten mit Strings und Numbers/97 Zufallszahlen/main.js
@@ -5,3 +5,8 @@
 let Zufallszahl = Math.random();
 console.log(Math.floor(Zufallszahl * 12 + 1));
 
+let min = 40;
+let max = 60;
+console.log(
+    Math.floor(Math.random() * (max - min + 1) + min)
+);
