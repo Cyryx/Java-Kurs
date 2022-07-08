f2af3d7e9f84f9dd4b34dcaff697bfd2d215e5c7
Jens Deubach <jens-d@live.de>
2022-07-08 11:20:17 +0200

    Methoden für Strings

---
+++ !Tutorials/Arbeiten mit Strings und Numbers/Methoden für Strings/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let s = " Lorem ipsum dolor sit amet ipsum. ";
+
+console.log(s.length);
+console.log(s.indexOf("ipsum", 20));
+console.log(s.lastIndexOf("ipsum", 10));
+console.log(s.includes("sit",20));
+console.log(s.toLocaleLowerCase());
+console.log(s.toLocaleUpperCase());
+console.log(s.trim());
+console.log(s.trimEnd());
+console.log(s.trimStart());
+console.log(s.repeat(25));
+
