2ce256c5c1938fc94465bed3e164b2c934600df9
Jens Deubach <jens-d@live.de>
2022-07-11 00:04:33 +0200

    106. Das Document-Objekt Eigenschaften und Methoden

---
+++ !Tutorials/!Document Objekt Model I/106. Das Document-Objekt Eigenschaften und Methoden/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log(document.body.innerHTML);
+//console.log(document.location.href = "https://www.google.de");
+console.log(document.location.protocol);
+console.log(document.URL);
+console.log(document.search);
