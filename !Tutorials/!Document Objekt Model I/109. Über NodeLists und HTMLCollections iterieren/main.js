5ab837972f8b40f03491d0e83a763138fe2a0356
Jens Deubach <jens-d@live.de>
2022-07-11 12:16:59 +0200

    Stopp 109. Über NodeLists und HTMLCollections iterieren

---
+++ !Tutorials/!Document Objekt Model I/109. Über NodeLists und HTMLCollections iterieren/main.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let html_collection_1 = document.getElementsByClassName("jumbotron");
+let html_collection_2 = document.getElementsByTagName("li");
+let node_list_1 = document.querySelectorAll("p");
+let node_list_2 = document.querySelectorAll("ul");
+//console.log(html_collection_1);
+
+console.log('**********For Schleife**********');
+for (let i = 0; i < html_collection_1.length; i++) {
+    console.log(html_collection_1[i]);
+}
+
+for (let i = 0; i < html_collection_2.length; i++) {
+    console.log(html_collection_2[i]);
+}
+
+console.log('**********For of Schleife**********');
+for (let e of html_collection_1) {
+    console.log(e);
+}
+
+for (let e of html_collection_2) {
+    console.log(e);
+}
+
+console.log('**********For Schleife Node List**********');
+for (let i = 0; i < node_list_1.length; i++) {
+    console.log(node_list_1[i]);
+    
+}
+for (let i = 0; i < node_list_2.length; i++) {
+    console.log(node_list_2[i]);
+    
+}
+
+console.log('********** ForOf Schleife Node List**********');
+for (let  e of node_list_1) {
+    console.log(e);
+}
+for (let  e of node_list_2) {
+    console.log(e);
+}
+
+console.log('**********forEaech Schleife Node List**********');
+node_list_1.forEach(function (e) {
+    console.log(e);
+})
+node_list_2.forEach(function (e) {
+    console.log(e);
+})
