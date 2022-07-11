45aa70ba9aef46f6aba317673d1631087037a852
Jens Deubach <jens-d@live.de>
2022-07-11 11:29:31 +0200

    107. Elemente auswählen I getElement...()-Methoden

--- !Tutorials/!Document Objekt Model I/107. Elemente auswählen I getElement...()-Methoden/main.js
+++ !Tutorials/!Document Objekt Model I/107. Elemente auswählen I getElement...()-Methoden/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+//Elemente nach Id Auswählen
+let el_by_id = document.getElementById("navigation");
+console.log(el_by_id);
+//Elemente nach ClassName  Auswählen
+let el_by_className_1 = document.getElementsByClassName("jumbotron");
+console.log(el_by_className_1);
+console.log(el_by_className_1.length !== 0);
+
+let el_by_className_2 = document.getElementsByClassName("teets");
+console.log(el_by_className_2);
+//Elemente nach TagName  Auswählen
+let el_by_tagName = document.getElementsByTagName("p");
+console.log(el_by_tagName);
