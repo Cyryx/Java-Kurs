6e5ee2bf2fd5fbaa9ad72bd79ab5c8b722b72d08
Jens Deubach <jens-d@live.de>
2022-07-24 00:12:16 +0200

    115

---
+++ !Tutorials/!Document Objekt Model I/114 - 115. Die Eigenschaften innerHTML, innerText und textContent I/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let jumbo = document.querySelector(".jumbotron");
+
+/* console.log(jumbo.innerHTML)
+console.log(jumbo.innerText)
+console.log(jumbo.textContent); */
+
+let string_1 = "";
+let string_2 = "<header><h2> Cillum fugiat excepteur consectetur velit do deserunt consequat voluptate excepteur esse duis ipsum.</h2></header>";
+let string_3 = "<p>Dolore ad nulla adipisicing culpa.</p>";
+let string_4 = "Id nisi quis et enim anim velit veniam elit Lorem amet."
+
+// jumbo.innerHTML = string_1;
+// jumbo.innerHTML = string_2;
+// jumbo.innerHTML += string_3;
+
+// jumbo.innerText = string_1;
+// jumbo.innerText = string_2
+
+// jumbo.textContent = string_1;
+// jumbo.textContent = string_2;
+jumbo.textContent = string_3;
--- !Tutorials/!Document Objekt Model I/114. Die Eigenschaften innerHTML, innerText und textContent I/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-let jumbo = document.querySelector(".jumbotron");
-
-console.log(jumbo.innerHTML)
-console.log(jumbo.innerText)
-console.log(jumbo.textContent);
