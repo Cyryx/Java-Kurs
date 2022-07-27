ad4d5252e22c9515747c0418e910ecb833211db6
Jens Deubach <jens-d@live.de>
2022-07-27 20:26:47 +0200

    Co-authored-by: MKW80 <MKW80@users.noreply.github.com>

---
+++ Michal/!Standard Template/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let array =
+    [
+        "Fleisch",
+        "Obst",
+        "banane"
+    ]
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+      
+  }
