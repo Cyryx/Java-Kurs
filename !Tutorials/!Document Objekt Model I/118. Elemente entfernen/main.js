3cc3ab48326141e457399bcd382a07277b6ecabc
Jens Deubach <jens-d@live.de>
2022-07-27 22:43:03 +0200

    118. Elemente entfernen

--- !Tutorials/!Document Objekt Model I/118. Elemente entfernen/main.js
+++ !Tutorials/!Document Objekt Model I/118. Elemente entfernen/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+
+let nav = document.querySelector("#navigation")
+
+nav.remove();
+
+let jumbo = document.querySelectorAll(".jumbotron");
+
+// jumbo.remove()
+
+jumbo.forEach(function (e) {
+    e.remove();
+})
