337e4783887d745b79526fca942f74da84d2227b
Jens Deubach <jens-d@live.de>
2022-08-26 10:53:01 +0200

    class navigation

--- !Praxis/Einnahmen+und+Ausgaben+unterscheiden/src/class/Navigationsleiste.js
+++ !Praxis/Einnahmen+und+Ausgaben+unterscheiden/src/class/Navigationsleiste.js
@@ -4,5 +4,34 @@
 //     <a href="index.html"><span id="markenname">Liqui-Planner</span></a>
 // </nav>
 class Navigationsleiste {
-    
+
+    constructor() {
+        this._html = this._html_generieren();
+    }
+
+    _html_generieren() {
+        let nav = document.createElement("nav");
+        nav.setAttribute("id", "navigationsleiste");
+
+        let a = document.createElement("a");
+        a.setAttribute("href", "#")
+
+        let span = document.createElement("span");
+        span.setAttribute("id", "markenname");
+        span.textContent = "Liqui-Planner";
+        a.insertAdjacentElement("afterbegin", span);
+        
+        nav.insertAdjacentElement("afterbegin", a);
+        
+        return nav
+    };
+
+    anzeigen() {
+
+        let body = document.querySelector("body");
+        if (body !== null) {
+            body.insertAdjacentElement("afterbegin",this._html);
+        }
+
+    }
 }
